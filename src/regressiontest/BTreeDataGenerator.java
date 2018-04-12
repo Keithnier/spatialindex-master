6e3e9c30c39f897d84bbd9295c696cd2746f5d3d
xinghen1995 <1097382820@qq.com>
2018-04-12 19:35:55 +0800

    BTreeDataGenerator在regressiontest/test3下生成了B树处理的数据集

---
+++ src/regressiontest/BTreeDataGenerator.java
@@ -0,0 +1,48 @@
+package regressiontest;
+
+import java.io.*;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.zip.GZIPOutputStream;
+
+public class BTreeDataGenerator {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Usage: fileName(数据文件名) docNum(文档数目) wordRange(关键词id范围)");
+        Scanner in = new Scanner(System.in);
+        String line = in.nextLine();
+        while(line == null || line.equals(""))
+            line = in.nextLine();
+        String[] params = line.split(" ");
+        String filepath = System.getProperty("user.dir") + File.separator + "src" +
+                File.separator + "regressiontest" + File.separator + "test3" + File.separator + params[0] + ".gz";
+        File file = new File(filepath);
+        if(file.exists()) {
+            file.delete();
+        }
+        file.createNewFile();
+        GZIPOutputStream gos = new GZIPOutputStream(new FileOutputStream(file));
+        PrintWriter pw = new PrintWriter(new OutputStreamWriter(new BufferedOutputStream(gos)));
+        int docNum = Integer.parseInt(params[1]);
+        int wordRange = Integer.parseInt(params[2]);
+        Random rand = new Random();
+        for(int i = 0; i < docNum; i++) {
+            StringBuilder line1 = new StringBuilder();
+            line1.append(String.valueOf(i));
+            line1.append(",");
+            line1.append(String.valueOf(rand.nextDouble()));
+            line1.append(",");
+            line1.append(String.valueOf(rand.nextDouble()));
+            line1.append(",");
+            int wordNum = rand.nextInt(20);
+            for(int j = 0; j < wordNum; j++) {
+                line1.append(String.valueOf(rand.nextInt(wordRange)));
+                line1.append(" ");
+                float f = rand.nextFloat();
+                line1.append(String.valueOf(rand.nextFloat()));
+                line1.append(",");
+            }
+            pw.println(line1.toString());
+        }
+        pw.close();
+    }
+}
